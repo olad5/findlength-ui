cae44e649389ed3024984b44af3c13d17f1d907f
Francis Shonubi <shonubioladipo@gmail.com>
2022-04-11 21:50:41 +0100

    feat:initial commit and connection with API
    - able to make requests to the backend

---
+++ components/App.js
@@ -0,0 +1,56 @@
+import {useState} from 'react'
+import { getVideoInfo } from "../services/InfoServices";
+
+function App() {
+  const [textInput, setTextInput] = useState("");
+
+  const handleChange = (event) => {
+    setTextInput(event.currentTarget.value);
+  };
+
+  function getAllInfo  (e) {
+    e.preventDefault()
+
+    getVideoInfo( textInput)
+      .then(info => {
+        console.log(info)
+      });
+  }
+
+
+
+
+  return (
+    <>
+      <div>
+        <div className="">
+                <form
+                  action=""
+                  className=""
+                >
+                  <div className="">
+                      <input
+                        type="text"
+                        placeholder="Enter link"
+                        className=""
+                        onChange={handleChange}
+                      />
+                    
+                  </div>
+                  <div className="">
+                    <button
+                      onClick={getAllInfo}
+                    >
+                      Get video
+                    </button>
+                  </div>
+                </form>
+              </div>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
+
