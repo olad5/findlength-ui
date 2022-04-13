09a069084a8c80ebca564bda3827f54281dffdfb
Francis Shonubi <shonubioladipo@gmail.com>
2022-04-13 17:52:13 +0100

    feat: add SpeedResult component

---
+++ components/atoms/speed-result/SpeedResult.tsx
@@ -0,0 +1,17 @@
+import SpeedResultProps from "./SpeedResult.d";
+
+const SpeedResult: React.FC<SpeedResultProps> = ({ data }) => {
+  return (
+    <div className="flex  items-center gap-2">
+      <img src="/icons/timer_black_fill.svg" alt="" />
+      <p className="">
+        At {data.speed}x:
+        <span className="ml-3 text-lg text-primary-veryDarkBlue">
+          {data.length}.
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default SpeedResult;
