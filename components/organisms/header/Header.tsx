130f8f78c8573e8e41f34381ed3a89a766fe1360
Francis Shonubi <shonubioladipo@gmail.com>
2022-04-14 14:56:10 +0100

    feat: add Header component

---
+++ components/organisms/header/Header.tsx
@@ -0,0 +1,14 @@
+const Header: React.FC = () => {
+  return (
+    <div className="m-auto w-2/4 ">
+      <p className="m-auto text-center text-6xl text-primary-veryDarkBlue">
+        Find the length of any Youtube{" "}
+        <span className="text-secondary-sand">video</span> or{" "}
+        <span className="text-secondary-sand">playlist</span> at different
+        playback speed!
+      </p>
+    </div>
+  );
+};
+
+export default Header;
