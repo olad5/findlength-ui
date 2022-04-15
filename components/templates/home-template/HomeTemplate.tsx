b4b65520e840241b599f263348dd2a4619fe7113
Francis Shonubi <shonubioladipo@gmail.com>
2022-04-15 19:35:07 +0100

    feat: add Footer to home Template

--- components/templates/home-template/HomeTemplate.tsx
+++ components/templates/home-template/HomeTemplate.tsx
@@ -1,4 +1,5 @@
 import CallToAction from "../../organisms/call-to-action/CallToAction";
+import Footer from "../../organisms/footer/Footer";
 import Header from "../../organisms/header/Header";
 import Instruction from "../../organisms/instructions/Instructions";
 import NavBar from "../../organisms/nav-bar/NavBar";
@@ -17,6 +18,9 @@ export default function HomeTemplate() {
       </div>
       <div className=" w-2/4 mx-auto  mt-16">
         <Instruction />
+      </div>
+      <div className="w-3/4 mx-auto mt-4 border-t-[1px] border-slate-200 py-6">
+        <Footer />
       </div>
     </div>
   );
