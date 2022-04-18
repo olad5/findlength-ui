05edc5043b53450e7125a417c60efaf85d6ae2dc
Francis Shonubi <shonubioladipo@gmail.com>
2022-04-18 09:41:30 +0100

    feat: click on app logo goes to home

--- components/organisms/nav-bar/NavBar.tsx
+++ components/organisms/nav-bar/NavBar.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return (
     <div className="flex justify-between w-3/4 m-auto items-center">
-      <a href="#" className="flex cursor-pointer items-center ">
-        <img src="/icons/play-btn-fill.svg" alt="" className="w-6" />
-        <p className="text-secondary-sand ml-2 text-2xl">Findlength</p>
-      </a>
+      <Link href="/">
+        <a className="flex cursor-pointer items-center ">
+          <img src="/icons/play-btn-fill.svg" alt="" className="w-6" />
+          <p className="text-secondary-sand ml-2 text-2xl">Findlength</p>
+        </a>
+      </Link>
       <div>
         <a href="#">
           <img
