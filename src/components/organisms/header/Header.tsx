124d942a27af3e1e21b44ef0a1744b30ca226610
Francis Shonubi <shonubioladipo@gmail.com>
2022-07-01 08:18:18 +0100

    style: add font syne for header

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -1,12 +1,12 @@
 export default function Header() {
   return (
     <div className="m-auto w-[70%] ">
-      <p className="m-auto text-center text-6xl text-primary-veryDarkBlue">
+      <h1 className="m-auto text-center text-6xl text-primary-veryDarkBlue font-Syne font-normal">
         Find the length of any Youtube{" "}
         <span className="text-secondary-sand">video</span> or{" "}
         <span className="text-secondary-sand">playlist</span> at different
         playback speed!
-      </p>
+      </h1>
     </div>
   );
 }
