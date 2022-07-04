f26ee5e625b8e0cb8df6e57aedb597bb4c2bb777
Francis Shonubi <shonubioladipo@gmail.com>
2022-07-04 19:36:00 +0100

    fix: error title should not be in _document.js
    - Putting the title in _document.js would show this <!----> in the page
      title. It would also log the error in the console.
    - Fix this by checking this
      https://nextjs.org/docs/messages/no-document-title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import "../styles/globals.css";
 import { AppWrapper } from "../context/AppContext";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Findlength</title>
+      </Head>
       <AppWrapper>
         <Component {...pageProps} />
       </AppWrapper>
--- pages/_document.js
+++ pages/_document.js
@@ -11,8 +11,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>Findlength </title>
-
           <link
             href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;700&display=swap"
             rel="stylesheet"
