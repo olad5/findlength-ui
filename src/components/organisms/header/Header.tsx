eeab84ef9c7aa02fba3a5962c71f6b55798693a2
Francis Shonubi <shonubioladipo@gmail.com>
2022-07-04 19:59:04 +0100

    style: made header text smaller

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <div className="m-auto w-[70%] ">
-      <h1 className="m-auto text-center text-6xl text-primary-veryDarkBlue font-Syne font-normal">
+    <div className="m-auto w-[50%] ">
+      <h1 className="m-auto text-center text-[3.1rem]  leading-[3.3rem] text-primary-veryDarkBlue font-Syne font-normal">
         Find the length of any Youtube{" "}
         <span className="text-secondary-sand">video</span> or{" "}
         <span className="text-secondary-sand">playlist</span> at different
